de4dc0ac37207c5212326e65b688ff94f6856022
Chris Roselle <chrisroselle@gmail.com>
2019-03-30 18:51:46 +0000

    Functioning python service

--- backend/service.py
+++ backend/service.py
@@ -1,13 +1,16 @@
 # credit to https://github.com/drhelius/docker-helloworld-python-microservice
 from flask import Flask
+from flask import request
 import os
 import socket
 
 app = Flask(__name__)
 
-@app.route('/api/test')
+@app.route('/api/test', methods=['POST'])
 def get_result():
+    content = request.get_json()
+    print(content)
     return 'test'
 
-if __name_ "__main__":
-    app.run(host="52.14.190.7", port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
